195c7d534d9ca42f87b688cddb4eab8cce7b0de5
Brett Mandler <brettmandler@gmail.com>
2019-03-28 09:49:26 -0400

    Mobile ui improvements

--- src/Containers/Navigation.jsx
+++ src/Containers/Navigation.jsx
@@ -4,9 +4,10 @@ import {
   BottomNavigationAction
 } from '@material-ui/core';
 import {
-  HomeTwoTone as HomeIcon,
-  SearchTwoTone as SearchIcon,
-  CategoryTwoTone as CategoryIcon
+  HomeOutlined as HomeIcon,
+  SearchOutlined as SearchIcon,
+  CategoryOutlined as CategoryIcon,
+  PollOutlined as TopIcon
 } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/styles';
 import { useCurrentRoute, useHistory } from 'react-navi';
@@ -14,7 +15,8 @@ import React from 'react';
 
 const useStyles = makeStyles(theme => ({
   label: {
-    fontSize: '0.5rem'
+    fontSize: '0.75rem',
+    fontWeight: '500'
   },
   root: {
     borderTop: {
@@ -24,10 +26,12 @@ const useStyles = makeStyles(theme => ({
     },
     bottom: 0,
     boxShadow: 'none',
+    fontSize: '1.8rem',
     top: 'auto'
   },
   selected: {
-    fontSize: '0.65rem !important'
+    fontSize: '0.75rem !important',
+    fontWeight: '600 !important'
   }
 }));
 
@@ -47,6 +51,12 @@ const Navigation = () => {
           icon={<HomeIcon />}
           label="Home"
           value="/"
+        />
+        <BottomNavigationAction
+          classes={{ label: classes.label, selected: classes.selected }}
+          icon={<TopIcon />}
+          label="Top apps"
+          value="/toplist/"
         />
         <BottomNavigationAction
           classes={{ label: classes.label, selected: classes.selected }}
