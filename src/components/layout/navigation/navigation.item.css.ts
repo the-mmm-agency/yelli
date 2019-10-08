4395b956fc973faf6d26899cabb39421da28b070
Brett Mandler <brettmandler@gmail.com>
2019-10-08 01:25:22 -0400

    Use styled-tools

--- src/components/layout/navigation/navigation.item.css.ts
+++ src/components/layout/navigation/navigation.item.css.ts
@@ -1,11 +1,15 @@
+import { ifProp } from 'styled-tools'
+
 import LinkBase from 'src/elements/link'
 import styled from 'src/util/styled'
 import { palette } from 'src/util/theme'
 
 export const Link = styled(LinkBase)<{
   selected?: boolean
-}>(({ selected, ...props }) => ({
-  color: palette(
-    selected ? 'primary.main' : 'text.secondary'
-  )(props),
-}))
+}>`
+  color: ${ifProp(
+    'selected',
+    palette('primary.main'),
+    palette('text.secondary')
+  )};
+`
